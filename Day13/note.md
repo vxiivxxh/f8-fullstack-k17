1f26794e4999a447389d09cb2cb53f49371217e7
VinhVu <thanhvinhvu.thpt@gmail.com>
2025-10-20 11:19:56 +0700

    Nội dung mới

---
+++ Day13/note.md
@@ -0,0 +1,16 @@
+# tailwind
+- Chèn thẳng vào html
+- Phương pháp class base: Chia các thuộc tính và giá trị của Css thành các class 
+- Ví dụ: text-red-600, font-bold,...
+
+# Có nên hcj SCSS k?
+Hiện nay CSS đã hỗ trợ nestes (cú pháp lồng)
+
+# Cachs học tailwind
+- Không học thuộc lòng
+- Nắm Các quy tắc
+- Một mẹo khi dùng tailwind trong html là tạo 1 file tailwind.config.js 
+
+Quy tắc:
+<!-- prefix-value (value này do tailwind build sẵn)
+prefix-[value] (value này do lâp trình viên thêm vào) -->
---
+++ Day14/js/ex01.js
@@ -0,0 +1,45 @@
+console.log("Học JS không khó");
+// Viết cmt 1 dòng
+
+/*
+Viết cmt nhiều dòng
+Viết cmt nhiều dòng
+Viết cmt nhiều dòng
+Viết cmt nhiều dòng
+*/
+
+// Biến: Dùng Để lưu trữ giá trị tamj thời (ở RAM)
+// Cú pháp: let tenbien hoặc let tenbien=giatri
+/*Quy ước đặt tên biến:
+-Chấp nhận: số, chữ thường, chữ hoa, gạch dưới và $
+-KHông đc bđ bằng số
+Chuẩn đặt tên:camelCase
+Vd: UserID, customerName, userShippingAddress,...
+userEmail, username
+*/
+// Lưu ý: Khi cùng 1 phạm vi, Không đc khai báo lại biến
+/*vd:
+let a = 10;
+let b;
+let c = "An";
+console.log(a);
+console.log(b);
+console.log(c);
+*/
+
+// let userID = 10;
+// let customer;
+// let customerName;
+
+// userID = 30;
+
+/*
+Hằng số:
+Quy tắc đặt tên giống như biến
+Cú pháp: const tenhang = giatri;
+Nếu hằng số đã xác định trc --> Đặt tên hằng là chữ hoa và nối bằng gạch dưới
+
+*/
+//vd:
+const fullname = "Thành Vinh";
+
