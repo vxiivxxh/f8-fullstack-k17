1787242133358159fcf467012a33fb4a3118984b
thanhvinhvu.thpt@gmail <thanhvinhvu.thpt@gmail.com>
2025-10-27 04:11:17 +0700

    Em mới làm bài 1 thôi ạ bài 2 em chưa hiểu lắm với em mới xem video đến bài cờ vua đang loay hoay ạ

---
+++ Day_16/js/ex.js
@@ -0,0 +1,19 @@
+//Bài 1:
+let n=5;
+let total= 0;
+
+for(let i=1; i<=n; i++){
+    total+=  i*(i+1);
+}
+console.log(total);
+
+//Bài 2:
+let a=5;
+let b=9;
+let isEven= 0;
+let isOdđ= 0;
+for(let i=a ; i<=b; i++){
+    if(i%2===0){
+    
+    }
+}
