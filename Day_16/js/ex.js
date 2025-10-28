5febd4aa31e9d98ba3f64ffc9f5ff78d8208957e
thanhvinhvu.thpt@gmail <thanhvinhvu.thpt@gmail.com>
2025-10-29 03:07:22 +0700

    Exercises

--- Day_16/js/ex.js
+++ Day_16/js/ex.js
@@ -64,3 +64,52 @@ for (let i = 6; i <= 10; i++) {
 htmlString += "</tr>";
 
 document.body.innerHTML = htmlString;
+
+//Bài4:Viết hàm tính tổng các số nguyên tố từ 1 đến n.
+
+//Lưu ý: Logic kiểm tra số nguyên tố tách thành 1 hàm khác. Có nghĩa sẽ có 2 hàm:
+
+function isPrime(n) {
+  if (n % 1 !== 0 || n <= 1) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+function getTotalPrime(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    if (isPrime(i)) {
+      total += i;
+    }
+  }
+  return total;
+}
+console.log(`Tổng các số nguyên tố từ 1 đến 10 = ${getTotalPrime(10)}`);
+
+//Bài 5:
+
+//Bài6: Viết hàm in ra số chính phương trong khoảng từ 1 đến n.
+function isCheckPerfectSquare(n) {
+  if (n < 0) {
+    return false;
+  }
+  for (let i = 1; i * i <= n; i++) {
+    if (i * i === n) {
+      return true;
+    }
+  }
+  return false;
+}
+function isPerfectSquare(n) {
+  for (let i = 1; i <= n; i++) {
+    if (isCheckPerfectSquare(i)) {
+      console.log(i);
+    }
+  }
+}
+isPerfectSquare(50);
