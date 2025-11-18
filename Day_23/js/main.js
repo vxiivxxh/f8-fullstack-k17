0e5a49184178ac711f050cb7aab59f16852eb563
thanhvinhvuthtq-code <thanhvinhvu.thtq@gmail.com>
2025-11-19 02:37:45 +0700

    exercise

--- Day_23/js/main.js
+++ Day_23/js/main.js
@@ -3,3 +3,16 @@ const username = document.querySelector("#username");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#check-password");
+
+// icon & error elements
+const iconUsername = document.querySelector("#icon-username");
+const errorUsername = document.querySelector("#error-username");
+
+const iconEmail = document.querySelector("#icon-email");
+const errorEmail = document.querySelector("#error-email");
+
+const iconPassword = document.querySelector("#icon-password");
+const errorPassword = document.querySelector("#error-password");
+
+const iconConfirm = document.querySelector("#icon-confirm");
+const errorConfirm = document.querySelector("#error-confirm");
