68bdf9d9d00537d5f59c38f075c590f5dfb69d86
thanhvinhvuthtq-code <thanhvinhvu.thtq@gmail.com>
2025-11-18 23:15:26 +0700

    updates

--- Day_23/js/main.js
+++ Day_23/js/main.js
@@ -1,1 +1,5 @@
-hgfhfg
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#check-password");
