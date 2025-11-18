ed2b572a0421bd6dcba46569dee1d3b49f57425c
thanhvinhvuthtq-code <thanhvinhvu.thtq@gmail.com>
2025-11-19 03:26:11 +0700

    exercise

--- Day_23/js/main.js
+++ Day_23/js/main.js
@@ -1,3 +1,4 @@
+//elements
 const form = document.querySelector("form");
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
@@ -16,3 +17,51 @@ const errorPassword = document.querySelector("#error-password");
 
 const iconConfirm = document.querySelector("#icon-confirm");
 const errorConfirm = document.querySelector("#error-confirm");
+
+// Lưu lại class gốc của input
+const usernameBaseClass = username.className;
+const emailBaseClass = email.className;
+const passwordBaseClass = password.className;
+const confirmPasswordBaseClass = confirmPassword.className;
+
+// Hàm validate email
+const validateEmailFormat = (email) => {
+  return email.includes("@");
+};
+
+//Hiển thị Error
+function showError(input, message, iconEl, errorEl, baseClass) {
+  // Reset về class gốc + border đỏ
+  input.className = baseClass + " border-red-500 bg-red-50 focus:ring-red-500";
+
+  //Hiện thông báo error
+  errorEl.textContent = message;
+}
+
+// validate email
+function validateEmail() {
+  const value = email.value.trim();
+
+  if (value === "") {
+    showError(
+      email,
+      "Email cannot be blank",
+      iconEmail,
+      errorEmail,
+      emailBaseClass
+    );
+    return false;
+  }
+
+  if (!validateEmailFormat(value)) {
+    showError(
+      email,
+      "Please enter a valid email",
+      iconEmail,
+      errorEmail,
+      emailBaseClass
+    );
+    return false;
+  }
+
+}
