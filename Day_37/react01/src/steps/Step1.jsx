cc02f9b9be781f1b4591e667c81ef97a7a2f8038
thanhvinhvuthtq-code <thanhvinhvu.thtq@gmail.com>
2026-01-24 16:11:35 +0700

    updates

--- Day_37/react01/src/steps/Step1.jsx
+++ Day_37/react01/src/steps/Step1.jsx
@@ -4,15 +4,15 @@ import FormInput from "../components/FormInput";
 export default function Step1() {
   return (
     <div>
-      <h2 className="text-3xl font-bold mb-8 text-white">Contact Info</h2>
+      <h2 className="text-3xl font-bold mb-8 text-white">Thông tin cá nhân</h2>
       <div className="grid grid-cols-2 gap-6">
         <FormInput
           name="firstName"
-          label="First Name"
+          label="Tên"
           placeholder="e.g. John"
         />
-        <FormInput name="lastName" label="Last Name" placeholder="e.g. Doe" />
-        <FormInput name="age" label="Age" type="number" placeholder="e.g. 18" />
+        <FormInput name="lastName" label="Họ" placeholder="e.g. Doe" />
+        <FormInput name="age" label="Tuổi" type="number" placeholder="e.g. 18" />
         <FormInput name="email" label="Email" placeholder="e.g. john@doe.com" />
       </div>
     </div>
